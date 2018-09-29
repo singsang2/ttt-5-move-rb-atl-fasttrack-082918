79778cbbc134b9c859def701a2ae036976976540
Sung Bae <singsang2@gmail.com>
2018-09-29 14:25:55 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,10 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
+i = 0
 # code your input_to_index and move method here!
 def input_to_index(move)
   piece = ["X", "O"]
   if board[move.to_i] == " "
-    board[move.to_i] = 
+    board[move.to_i] = piece[move.to_i%2]
 end
